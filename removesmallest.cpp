e06e0dae79ce20ea8cffefd5ed85cd6c0abfe04f
Gobson995 <gugabadazanatta@gmail.com>
2026-04-10 20:49:10 -0300

    questão do dia feita

---
+++ removesmallest.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int t; cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+
+        vector<int> a(n);
+
+        for (int i =0; i < n; i++) cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        bool opa = true;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] - a[i-1] > 1) {
+                opa = false;
+                break;
+            }
+        }
+
+        if (opa) cout << "YES";
+        else cout << "NO";
+
+        cout << endl;
+    }
+}
