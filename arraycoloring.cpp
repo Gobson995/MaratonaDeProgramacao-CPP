2f3737643883dc7173892613b4a6c5c2fd2ea45d
Gobson995 <gugabadazanatta@gmail.com>
2026-04-28 21:19:53 -0300

    questão do dia feita

---
+++ arraycoloring.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int t; cin >> t;
+
+    while (t--) {
+        int n; cin >> n;
+
+        int x;
+        int impares = 0;
+
+        for (int i = 0; i < n; i++) {
+            cin >> x;
+            if (x % 2 != 0) impares++;
+        }
+
+        if (impares % 2 == 0) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
