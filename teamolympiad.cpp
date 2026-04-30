00f9bbdc28f95f07a40ab6d0d4494935b0ddf617
Gobson995 <gugabadazanatta@gmail.com>
2026-04-30 19:01:25 -0300

    questão do dia feita

---
+++ teamolympiad.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int n; cin >> n;
+
+    vector<int> prog, math, pe;
+
+    for (int i = 1; i <= n; i++) {
+        int t; cin >> t;
+
+        if (t == 1) prog.push_back(i);
+        else if (t == 2) math.push_back(i);
+        else pe.push_back(i);
+    }
+
+    int w = min({(int)prog.size(), (int)math.size(), (int)pe.size()});
+
+    cout << w << endl;
+
+    for (int i = 0; i < w; i++) {
+        cout << prog[i] << " " << math[i] << " " << pe[i] << endl;
+    }
+}
